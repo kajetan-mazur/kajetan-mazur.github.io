2b58fda4aa3d379e32c7c51a2b0b5cfc09b5e217
Kajetan Mazur <kajetan@webownia.pl>
2017-07-13 13:31:43 +0200

    adding little style-ish and some functionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 var btnAddActivity = document.getElementById('btnAddActivity');
 btnAddActivity.addEventListener('click', function(){createForm();});
+var num =1;
 function createForm(){
   var divForm = document.getElementById('divForm');
-  divForm.innerHTML='<form><label>Plan dnia</label><input></form>';
+  divForm.innerHTML='<form id="form"><label id="label">What i have to do today?</label><br/><input id="divFormInput"><br/><button type="submit" id="divFormSubmit">Submit</button></form>';
+  var divFormSubmit = document.getElementById('divFormSubmit');
+  divFormSubmit.addEventListener('click', function(){addDayPlan(event);});
+}
+
+function addDayPlan(){
+  event.preventDefault();
+  var dayPlan = document.getElementById('dayPlan');
+  var divFormInput = document.getElementById('divFormInput').value;
+  dayPlan.innerHTML += `<p>${num}.${divFormInput}</p>`;
+  num++;
 }
